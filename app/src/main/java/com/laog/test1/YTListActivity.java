9a1612100e51e95f04073049516a61f08408b8eb
lulugyf <gyf.freedom@gmail.com>
2018-12-25 10:46:04 +0800

    delete .idea

--- app/src/main/java/com/laog/test1/YTListActivity.java
+++ app/src/main/java/com/laog/test1/YTListActivity.java
@@ -25,70 +25,70 @@ public class YTListActivity extends Activity implements View.OnClickListener {
 
     private final String TAG = "YT";
 
-        ArrayList<DataModel> dataModels;
-        ListView listView;
-        private static CustomAdapter adapter;
+    ArrayList<DataModel> dataModels;
+    ListView listView;
+    private static CustomAdapter adapter;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.ag_activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ag_activity_main);
 
-            listView = (ListView)findViewById(R.id.list);
+        listView = (ListView)findViewById(R.id.list);
 
-            dataModels= new ArrayList<>();
+        dataModels= new ArrayList<>();
 
-            dataModels.add(new DataModel("Apple Pie", "Android 1.0", "1","September 23, 2008"));
-            dataModels.add(new DataModel("Banana Bread", "Android 1.1", "2","February 9, 2009"));
-            dataModels.add(new DataModel("Cupcake", "Android 1.5", "3","April 27, 2009"));
-            dataModels.add(new DataModel("Donut","Android 1.6","4","September 15, 2009"));
-            dataModels.add(new DataModel("Eclair", "Android 2.0", "5","October 26, 2009"));
-            dataModels.add(new DataModel("Froyo", "Android 2.2", "8","May 20, 2010"));
-            dataModels.add(new DataModel("Gingerbread", "Android 2.3", "9","December 6, 2010"));
-            dataModels.add(new DataModel("Honeycomb","Android 3.0","11","February 22, 2011"));
-            dataModels.add(new DataModel("Ice Cream Sandwich", "Android 4.0", "14","October 18, 2011"));
-            dataModels.add(new DataModel("Jelly Bean", "Android 4.2", "16","July 9, 2012"));
-            dataModels.add(new DataModel("Kitkat", "Android 4.4", "19","October 31, 2013"));
-            dataModels.add(new DataModel("Lollipop","Android 5.0","21","November 12, 2014"));
-            dataModels.add(new DataModel("Marshmallow", "Android 6.0", "23","October 5, 2015"));
+        dataModels.add(new DataModel("Apple Pie", "Android 1.0", "1","September 23, 2008"));
+        dataModels.add(new DataModel("Banana Bread", "Android 1.1", "2","February 9, 2009"));
+        dataModels.add(new DataModel("Cupcake", "Android 1.5", "3","April 27, 2009"));
+        dataModels.add(new DataModel("Donut","Android 1.6","4","September 15, 2009"));
+        dataModels.add(new DataModel("Eclair", "Android 2.0", "5","October 26, 2009"));
+        dataModels.add(new DataModel("Froyo", "Android 2.2", "8","May 20, 2010"));
+        dataModels.add(new DataModel("Gingerbread", "Android 2.3", "9","December 6, 2010"));
+        dataModels.add(new DataModel("Honeycomb","Android 3.0","11","February 22, 2011"));
+        dataModels.add(new DataModel("Ice Cream Sandwich", "Android 4.0", "14","October 18, 2011"));
+        dataModels.add(new DataModel("Jelly Bean", "Android 4.2", "16","July 9, 2012"));
+        dataModels.add(new DataModel("Kitkat", "Android 4.4", "19","October 31, 2013"));
+        dataModels.add(new DataModel("Lollipop","Android 5.0","21","November 12, 2014"));
+        dataModels.add(new DataModel("Marshmallow", "Android 6.0", "23","October 5, 2015"));
 
-            adapter = new CustomAdapter(dataModels, getApplicationContext());
+        adapter = new CustomAdapter(dataModels, getApplicationContext());
 
-            listView.setAdapter(adapter);
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                    DataModel dataModel= dataModels.get(position);
+                DataModel dataModel= dataModels.get(position);
 
-                    Snackbar.make(view, dataModel.getName()+"\n"+dataModel.getType()+" API: "+dataModel.getVersion_number(), Snackbar.LENGTH_LONG)
-                            .setAction("No action", null).show();
-                }
-            });
+                Snackbar.make(view, dataModel.getName()+"\n"+dataModel.getType()+" API: "+dataModel.getVersion_number(), Snackbar.LENGTH_LONG)
+                        .setAction("No action", null).show();
+            }
+        });
 
-            ((Button)findViewById(R.id.button)).setOnClickListener(this);
-        }
+        ((Button)findViewById(R.id.button)).setOnClickListener(this);
+    }
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            getMenuInflater().inflate(R.menu.ag_menu_main, menu);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.ag_menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
             return true;
         }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            int id = item.getItemId();
-
-            //noinspection SimplifiableIfStatement
-            if (id == R.id.action_settings) {
-                return true;
-            }
-            if(id == R.id.action_rx){
-                rxtest();
-            }
-
-            return super.onOptionsItemSelected(item);
+        if(id == R.id.action_rx){
+            rxtest();
         }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     /**
      * https://www.coderefer.com/rxandroid-tutorial-getting-started/
@@ -96,31 +96,34 @@ public class YTListActivity extends Activity implements View.OnClickListener {
      * https://medium.com/@kurtisnusbaum/rxandroid-basics-part-1-c0d5edcf6850
      */
     private void rxtest() {
-            Observable<String> stringObservable = Observable.just("Hello Reactive Programming!");
+        Observable<String> stringObservable = Observable.just("Hello Reactive Programming!");
 
-            Observer<String> stringObserver = new Observer<String>() {
-                @Override
-                public void onSubscribe(Disposable d) {
-                }
+        Observer<String> stringObserver = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+            }
 
-                @Override
-                public void onNext(String s) {
-                    Toast.makeText(YTListActivity.this, s, Toast.LENGTH_SHORT).show();
-                    Log.d(TAG, "---haha:"+s);
-                }
+            @Override
+            public void onNext(String s) {
+                toast(s);
+                Log.d(TAG, "---haha:"+s);
+            }
 
-                @Override
-                public void onError(Throwable e) {
-                }
+            @Override
+            public void onError(Throwable e) {
+            }
 
-                @Override
-                public void onComplete() {
-                }
-            };
+            @Override
+            public void onComplete() {
+            }
+        };
 
-            stringObservable.subscribe(stringObserver);
-        }
+        stringObservable.subscribe(stringObserver);
+    }
 
+    private void toast(String msg) {
+        Toast.makeText(YTListActivity.this, msg, Toast.LENGTH_SHORT).show();
+    }
 
     Observable mObservable;
     Observer<Integer> mObserver;
@@ -142,22 +145,22 @@ public class YTListActivity extends Activity implements View.OnClickListener {
         mObserver = new Observer<Integer>() {
             @Override
             public void onSubscribe(Disposable d) {
-                Toast.makeText(YTListActivity.this, "onSubscribe called", Toast.LENGTH_SHORT).show();
+                toast("onSubscribe called");
             }
 
             @Override
             public void onNext(Integer integer) {
-                Toast.makeText(YTListActivity.this, "onNext called: " + integer, Toast.LENGTH_SHORT).show();
+                toast("onNext called: " + integer);
             }
 
             @Override
             public void onError(Throwable e) {
-                Toast.makeText(YTListActivity.this, "onError called", Toast.LENGTH_SHORT).show();
+                toast("onError called");
             }
 
             @Override
             public void onComplete() {
-                Toast.makeText(YTListActivity.this, "onComplete called", Toast.LENGTH_SHORT).show();
+                toast("onComplete called");
             }
         };
     }
